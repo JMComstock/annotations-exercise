12b0cfa85893e6f7bb2c1560586505a599fd59dc
JMComstock <jasoncomstock11@gmail.com>
2022-03-03 14:52:31 -0600

    added ArrayList<String> to the list of books, removed getBooks method

--- src/Author.java
+++ src/Author.java
@@ -5,11 +5,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Author extends Person {
-    private List books;
+    private ArrayList<String> books;
 
     public Author(String firstName, String lastName) {
         super(firstName, lastName);
-        books = new ArrayList();
+        books = new ArrayList<>();
     }
 
     /**
@@ -17,19 +17,22 @@ public class Author extends Person {
      */
     @Deprecated
     public List<String> getBooks() {
+
         return books;
     }
 
     public List<String> publishedBooks() {
+
         return books;
     }
 
     public void addBook(String book) {
+
         books.add(book);
     }
 
-    @Override
     public String sortName() {
+
         return String.format("%s, %s", lastName, firstName);
     }
 }
